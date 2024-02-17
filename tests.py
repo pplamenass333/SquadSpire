73621a249b1906ea2148abe8c82b39fe29732cd0
Viktor <126186961+VektorOO7@users.noreply.github.com>
2024-02-17 23:56:40 +0200

    fixed review test

--- tests.py
+++ tests.py
@@ -6,20 +6,20 @@ import unittest
 class TestReviewClass(unittest.TestCase):
 
     def test_review_creation(self):
-        review = Review("user1", 4, "Great product!")
+        review = Review("user1", 4, "Great event!")
 
         self.assertEqual(review.username(), "user1")
         self.assertEqual(review.rating(), 4)
-        self.assertEqual(review.comment(), "Great product!")
+        self.assertEqual(review.comment(), "Great event!")
         self.assertFalse(review.is_edited())
 
     def test_edit_comment(self):
-        review = Review("user2", 3, "Good product!")
-        
+        review = Review("user2", 3, "Good event!")
+
         self.assertFalse(review.is_edited())
-        
+
         review.edit_comment("Updated comment!")
-        
+
         self.assertEqual(review.comment(), "Updated comment!")
         self.assertTrue(review.is_edited())
 
