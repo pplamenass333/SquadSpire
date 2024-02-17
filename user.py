c8a47e503ad3968c9adecd9b478783db01518e6d
Viktor <126186961+VektorOO7@users.noreply.github.com>
2024-02-17 19:58:56 +0200

    Completed the User class

--- user.py
+++ user.py
@@ -1,24 +1,46 @@
 
 
+class Gender(Enum):
+    MALE
+    FEMALE
+    OTHER
+
+
 class User:
-    def __init__(self, name, age, gender, profile_picture, description, hobbies, interests):
+    def __init__(self, name, age, gender, description, hobbies, interests):
         self.name = name
         self.age = age
         self.gender = gender
-        self.profile_picture = profile_picture
         self.description = description
         self.hobbies = hobbies
         self.interests = interests
-        
-        self.history = []
-        self.reviews = []
-        self.friends = []
 
+        self.__history = []
+        self.__reviews = []
+        self.__friends = []
+
+    #  getters
+    def history(self):
+        return self.__history
+
+    def reviews(self):
+        return self.__reviews
+
+    def friends(self):
+        return self.__friends
+
+    #  methods
     def add_event_to_history(self, event):
         self.history.append(event)
 
     def add_review(self, review):
         self.reviews.append(review)
 
+    def remove_review(self, review_id):
+        self.reviews.pop(review_id)
+
     def add_friend(self, username):
         self.friends.append(username)
+
+    def remove_friend(self, username):
+        self.friends.remove(username)
